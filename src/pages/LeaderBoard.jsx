c48bcde7c984d3635ab5382ca284dde997dc9bfd
AkilaInduwara <akilainduwara@gmail.com>
2025-04-03 11:59:53 +0530

    Update LeaderBoard.jsx

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -238,13 +238,13 @@ const LeaderBoard = () => {
               <path d="M0 0h24v24H0z" fill="none" />
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
             </svg>
-            <div className="user-tooltip-ldrb">
+            {/* <div className="user-tooltip-ldrb">
               <h3>{loading ? "Loading..." : playerName}</h3>
               <p>Rank: 1</p>
               <p>Level: 5</p>
               <p>Total Score: 000</p>
               <p>Games Played: 10</p>
-            </div>
+            </div> */}
           </div>
           <div className="user-name-ldrb">
             {loading ? (
